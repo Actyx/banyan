333b0a68e1831aa05ccdccc175c3fa7564f85e78
Ruediger Klaehn <rklaehn@protonmail.com>
2020-06-07 11:07:14 +0200

    Remove some old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-// some old experiments
-#[allow(dead_code, unused_imports)]
-mod czaa;
-
-// some new experiments
-#[allow(dead_code, unused_imports)]
-mod flat_tree;
-
 pub mod index;
 pub mod ipfs;
 pub mod store;
