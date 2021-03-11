ef962f5fadd10bf7dc89a9320901f6f9f6885c5b
Oliver Wangler <oliver@wngr.de>
2021-03-11 20:37:29 +0000

    use rustc 1.50.0

--- banyan/src/util.rs
+++ banyan/src/util.rs
@@ -80,6 +80,7 @@ where
     Self: 'static + Sized + Send,
     Self::Item: Send,
 {
+    #[allow(clippy::clippy::wrong_self_convention)]
     fn to_stream(self, buffer_size: usize, thread_pool: ThreadPool) -> mpsc::Receiver<Self::Item> {
         let (mut sender, receiver) = mpsc::channel(buffer_size);
 
